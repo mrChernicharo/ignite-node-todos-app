ce383b39d00b285bcd8069ed3a31ebe555b241bc
Felipe Chernicharo <string7dev@gmail.com>
2021-03-10 17:19:23 -0300

    update TODOS

--- src/index.js
+++ src/index.js
@@ -91,6 +91,23 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+
+  if (new Date(deadline).getTime() <= new Date().getTime()) {
+    return response.json({
+      error:
+        "a data da deadline precisa ser maior que a data de criação do TODO",
+    });
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json({ message: "TODO atualizado com sucesso!", todo });
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
