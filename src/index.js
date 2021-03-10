26ecea260e3d10212cfd65c4d4e79eafb9334c17
Felipe Chernicharo <string7dev@gmail.com>
2021-03-10 16:54:15 -0300

    requests inicias

--- src/index.js
+++ src/index.js
@@ -1,40 +1,62 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
-app.post('/users', (request, response) => {
+app.post("/users", (request, response) => {
+  // Complete aqui
+  const { name, username } = request.body;
+
+  const newUser = {
+    id: uuid(),
+    name,
+    username,
+    todos: [],
+  };
+
+  if (users.some((user) => user.username === newUser.username)) {
+    return response.status(405).json({
+      error: `Esse Username já está sendo utilizado. Escolha outro por favor`,
+    });
+  }
+
+  users.push(newUser);
+
+  return response.json(newUser);
+});
+
+app.get("/all-users", (request, response) => {
+  return response.json(users);
+});
+
+app.get("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
+app.post("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
